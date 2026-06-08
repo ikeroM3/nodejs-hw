1be2a822a63c5c5168f927497ef518e7d13956f2
Artem <stuzukartem12@gmail.com>
2026-06-08 20:57:55 +0300

    03-validation

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -5,7 +5,7 @@ import {
   createNote,
   deleteNote,
   updateNote,
-} from '../controllers/noteController.js';
+} from '../controllers/notesController.js';
 import { celebrate } from 'celebrate';
 import {
   getAllNotesSchema,
