c611a347e2824b9ded9003b99f4bb14b043064d8
Artem <stuzukartem12@gmail.com>
2026-06-21 19:57:36 +0300

    05.v6

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -147,6 +147,7 @@ export const requestResetEmail = async (req, res) => {
 
   try {
     await sendEmail({
+      from: process.env.SMTP_FROM,
       to: email,
       subject: 'Скидання паролю',
       html,
